b365a551b595a9a2769b8537b1ad4bf9f6f45c31
Lucas Martins <lucas.martins@ed.company>
2019-12-13 15:09:04 -0300

    Adicionada barra de navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PessoaComponent } from './pessoa/pessoa.component';
+import { EstadoComponent } from './estado/estado.component';
+import { PaisComponent } from './pais/pais.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PessoaComponent,
+    EstadoComponent,
+    PaisComponent
   ],
   imports: [
     BrowserModule
